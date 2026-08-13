3b4535dc20e679a0a3d5a28e036036163e3446bb
juanigfm <juanigfm@gmail.com>
2026-08-13 00:25:40 -0300

    Missing arrows movement handle and code legibility improvement

--- Assets/TP1/Scripts/Movement.cs
+++ Assets/TP1/Scripts/Movement.cs
@@ -6,37 +6,42 @@ public class Movement : MonoBehaviour
 {
     float speed = 2f;
     float sprintModifier = 2f;
-
     private SpriteRenderer spriteRenderer;
     private Color itemColor;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         spriteRenderer = GetComponent <SpriteRenderer>();
         itemColor = Color.white;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Movement handle.
-        float step = Input.GetKey(KeyCode.LeftShift) ? speed * Time.deltaTime * sprintModifier : speed * Time.deltaTime; // Runs with Shift pressed.
+        HandleMovement();
+        HandleColor();
+    }
 
-        if (Input.GetKey(KeyCode.W))
+    private void HandleMovement()
+    {
+        float step = Input.GetKey(KeyCode.LeftShift) ? speed * Time.deltaTime * sprintModifier : speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
             transform.Translate(Vector3.up * step);
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
             transform.Translate(Vector3.left * step);
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
             transform.Translate(Vector3.down * step);
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             transform.Translate(Vector3.right * step);
+
         if (Input.GetKeyDown(KeyCode.Q))
             transform.Rotate(new Vector3(0, 0, 10));
         if (Input.GetKeyDown(KeyCode.E))
             transform.Rotate(new Vector3(0, 0, -10));
+    }
 
-        //Color handle.
+    private void HandleColor()
+    {
         if (Input.GetKeyDown(KeyCode.O)) // Hides when O is pressed.
             spriteRenderer.color = Color.clear;
         if (Input.GetKeyUp(KeyCode.O)) // Stops hidding when key is up.
